cbf6ebaddc5ddc3806c2a370669f1e86f2f041a6
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-07-02 13:34:14 +0000

    Your commit message

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+  "I'm a frontend developer at Delhi University. I enjoy building dynamic and user-friendly web applications with a focus on clean and efficient code.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML and CSS",
+  "JavaScript",
+  "React",
+  "Responsive design",
+  "API integration",
+  "Git and Github"
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am dedicated to building dynamic and responsive web applications that provide seamless user experiences. With expertise in frontend technologies, I strive to create visually appealing and efficient interfaces that enhance user engagement and accessibility.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,27 +31,23 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+    title: "10 Things to know about Azure Static Web Apps 🎉",
+    description: "Collaboration to create a beginner friendly....",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
     title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    description: "Contributed sketch note imagery to accompany...",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
     title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    description: "Created from Microsoft's resume workshop...",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    title: "GitHub Codespaces and GitHub.dev",
+    description: "Video interview to explain when to use GitHub.dev...",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
